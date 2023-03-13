6c1445313ad1419c2d615e55610237a6eb7226ef
Kimberley Ni <wukimberley98@gmail.com>
2023-03-13 10:06:18 -0400

    interfaces added

---
+++ src/main/java/vehicle/SelfDriving.java
@@ -0,0 +1,16 @@
+package vehicle;
+
+public interface SelfDriving {
+    /** @throws IllegalArgumentException if miles is negative.*/
+    public void driveAutonomously(double miles);
+}
+
+interface Flying {
+    /** @throws IllegalArgumentException if miles is negative.*/
+    public boolean canFly(double miles);
+
+    /** @throws IllegalArgumentException if miles is negative.
+    @throws IllegalArgumentException if miles exceeds the remaining
+    range of the car.*/
+    public void fly(double miles);
+}
