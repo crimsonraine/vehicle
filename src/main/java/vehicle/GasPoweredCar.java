55a7acdec6a7685e1f9bc1c0a426e8a508c7b300
ParJai <parthjain22@gmail.com>
2023-03-16 10:26:58 -0400

    Fixed GasPowered drive recursion issue

--- src/main/java/vehicle/GasPoweredCar.java
+++ src/main/java/vehicle/GasPoweredCar.java
@@ -39,7 +39,7 @@ public class GasPoweredCar extends Car {
     public void drive(double miles) {
         if (miles < 0) throw new IllegalArgumentException();
         if (canDrive(miles)) {
-            drive(miles);
+            addMileage(miles);
             decreaseFuelLevel(miles);
         } else {
             throw new IllegalArgumentException();
